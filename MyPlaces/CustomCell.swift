f98eb03da3eff64bd1f94b91af1d9e52c66e2a7b
Berestnev <berestnev.sv@gmail.com>
2021-07-24 01:12:30 +0300

    started changing the table view of the image

---
+++ MyPlaces/CustomCell.swift
@@ -0,0 +1,22 @@
+//
+//  CustomCell.swift
+//  MyPlaces
+//
+//  Created by Владимир on 23.07.2021.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var imagePlace: UIImageView!
+    
+    @IBOutlet weak var imageCategory: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+
+}
+
--- MyPlaces/MainTableVC.swift
+++ MyPlaces/MainTableVC.swift
@@ -4,13 +4,12 @@ import UIKit
 class MainTableVC: UITableViewController {
     
     let places: [String] = ["KFC","McDonalds",
-                            "Burger Heroes","ПиццаСушиВок",
-                            "Тануки","Северяне",
+                            "Burger Heroes","Якитория",
+                            "Тануки","Рыба моя",
                             "ЙУХ","Хачапури и Вино"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
 
     // MARK: - Table view data source
@@ -20,18 +19,18 @@ class MainTableVC: UITableViewController {
         return places.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
-        cell.textLabel?.text = places[indexPath.row]
-        cell.imageView?.image = UIImage(named: places[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.size.height / 2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath) as! CustomCell
+        cell.nameLabel?.text = places[indexPath.row]
+        cell.imagePlace?.image = UIImage(named: places[indexPath.row])
+        cell.imagePlace?.layer.cornerRadius = 10
+        cell.imageCategory.image = UIImage(named: "Ролл 1")
         return cell
     }
     
     
     // MARK: - TableViewDelegate
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 60 
+        return 210
     }
     
     
